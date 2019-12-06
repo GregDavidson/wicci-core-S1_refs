cea7c541816f7c799bc6b4912cb9f7687aeda522
GregDavidson <greg.davidson+git@gmail.com>
2019-12-05 20:13:47 -0800

    heading back to normal

--- settings.sql
+++ settings.sql
@@ -5,20 +5,19 @@
 
 SELECT set_schema_path('s1_refs','s0_lib','public');
 SELECT spx_debug_on(); -- jgd debugging!!
-SELECT spx_test_select('SELECT ''About to call spx_collate_locate''', 0);
--- SELECT ensure_schema_ready();
+SELECT ensure_schema_ready();
 -- these things don't really exist until part way through this schema
 -- so I've defined dummy versions of them that do nothing at the
 -- end of ../S0_lib/s0-lib.sql
--- all of these seem to kill the server - am I calling the wrong one??
-SELECT spx_collate_locale();
-SELECT spx_test_select('SELECT ''About to call unsafe_spx_load_schemas''', 0);
-SELECT unsafe_spx_load_schemas();
-SELECT spx_test_select('SELECT ''About to call unsafe_spx_load_schema_path''', 0);
-SELECT unsafe_spx_load_schema_path();
-SELECT spx_test_select('SELECT ''About to call unsafe_spx_load_types''', 0);
-SELECT unsafe_spx_load_types();
-SELECT spx_test_select('SELECT ''About to call unsafe_spx_load_procs''', 0);
-SELECT unsafe_spx_load_procs();
-SELECT spx_test_select('SELECT ''About to call unsafe_spx_initialize''', 0);
-SELECT unsafe_spx_initialize();
+-- SELECT spx_test_select('SELECT ''About to call spx_collate_locate''', 0);
+-- SELECT spx_collate_locale();
+-- SELECT spx_test_select('SELECT ''About to call unsafe_spx_load_schemas''', 0);
+-- SELECT unsafe_spx_load_schemas();
+-- SELECT spx_test_select('SELECT ''About to call unsafe_spx_load_schema_path''', 0);
+-- SELECT unsafe_spx_load_schema_path();
+-- SELECT spx_test_select('SELECT ''About to call unsafe_spx_load_types''', 0);
+-- SELECT unsafe_spx_load_types();
+-- SELECT spx_test_select('SELECT ''About to call unsafe_spx_load_procs''', 0);
+-- SELECT unsafe_spx_load_procs();
+-- SELECT spx_test_select('SELECT ''About to call unsafe_spx_initialize''', 0);
+-- SELECT unsafe_spx_initialize();
